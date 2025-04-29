d7df1fd36dd41d3545016c41f73063d8cde9c224
aidiloj <muhamad.aidil@ottojoie.com>
2025-04-29 14:12:07 +0800

    create daily log table

---
+++ database/migrations/2025_04_29_060946_create_daily_logs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('daily_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->json('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daily_logs');
+    }
+};
