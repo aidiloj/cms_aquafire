bacafe2a64fd575d07fded857c6ca3666102b4f5
aidiloj <muhamad.aidil@ottojoie.com>
2025-04-30 09:47:34 +0800

    aaa

--- resources/views/livewire/daily-log.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @livewireStyles
-
-    hell
-
-    @livewireScripts
-</div>
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('log', DailyLog::class);
+// Route::get('log', DailyLog::class);
