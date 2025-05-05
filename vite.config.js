7698ddea93c6b68d6436ea3ff3ce414350bdb128
aidiloj <muhamad.aidil@ottojoie.com>
2025-05-05 10:17:48 +0800

    get images working

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
+            transformOnServe: (code) => code.replaceAll('/images', 'http://localhost:8000/images'),
         }),
         tailwindcss(),
     ],
