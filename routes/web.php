6d40525ec2147198c2d8f924e37cab6583fcfc37
aidiloj <muhamad.aidil@ottojoie.com>
2025-05-05 10:09:49 +0800

    init template

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Livewire\DailyLog;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BlogController::class,'index'])->name('home'); 
+
 
 // Route::get('log', DailyLog::class);
